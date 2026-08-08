c428d1edf39a59fe778fb4181855a9ed9462cf9f
Santiago Panozzo <santiagopanozzoa@outlook.com>
2026-08-08 18:48:39 -0300

    fix: re-export FlowResult in flows __init__

--- app/flows/__init__.py
+++ app/flows/__init__.py
@@ -1,5 +1,7 @@
 from .activities import ActivitiesFlow
-from .base import BaseFlow
+from .base import BaseFlow, FlowResult
+
+__all__ = ["FLOW_REGISTRY", "BaseFlow", "FlowResult", "ActivitiesFlow"]
 
 FLOW_REGISTRY: dict[str, BaseFlow] = {
     "activities": ActivitiesFlow(),
